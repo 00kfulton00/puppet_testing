4f0f6835490a4b6f93315ea695058e28234e9b41
00kfulton00 <45300609+00kfulton00@users.noreply.github.com>
2019-07-08 11:24:48 -0400

    Update init.pp

--- modules/nac/manifests/init.pp
+++ modules/nac/manifests/init.pp
@@ -2,4 +2,17 @@
 class nac (
 ) {
   include zabbix::sender
-  }
+  
+  class { 'python':
+    version     => 'system',
+    pip         => 'present',
+    dev         => 'absent',
+    virtualenv  => 'absent',
+    gunicorn    => 'absent',
+    }
+    
+    python::pip { 'packaging' :
+      pkgname     => 'packaging',
+      ensure      => '19.0',
+      }
+ }

da5a50fdcf270a7cc62c671fdfd7789737003224
00kfulton00 <45300609+00kfulton00@users.noreply.github.com>
2019-07-08 12:12:30 -0400

    Update init.pp

--- modules/nac/manifests/init.pp
+++ modules/nac/manifests/init.pp
@@ -4,7 +4,7 @@ class nac (
   include zabbix::sender
   
   class { 'python':
-    version     => 'python33'
+    version     => 'python33',
     pip         => 'latest',
     dev         => 'absent',
     virtualenv  => 'absent',
